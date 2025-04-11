07e473a84fa52b29527bfbfc2f4ffa31d271278f
baltichaima <chaimabalti32@gmail.com>
2025-04-11 10:09:51 +0200

    First jenkinsTP commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+	@Test
+	void contextLoads() {
+
+   void contextLoads() {
+	 system.out.println(" it is a test class ") ;
+	 assserequals(expected true , actual true ) ;
+   }
+
+	
+	}
+}
